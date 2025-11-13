695e30f1949bc803167caf72c1659643a4f2d0c2
Oui42 <ouix42@gmail.com>
2025-11-13 19:12:06 +0100

    Add Courses LMS plugin v0.3.0 with exam builder, PDF certificates and GitHub Actions build workflow

---
+++ courses/courses.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Courses
+Plugin URI: http://example.com/
+Description: A plugin for managing courses.
+Version: 0.3.0
+Author: Oui42
+Author URI: http://example.com/
+*/
+
+// Course Plugin Code Here
+?>
